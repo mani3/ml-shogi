887298719cd483ebdd712539323875011d91eb6b
mani3 <kazuya.4da@gmail.com>
2020-11-02 01:25:42 +0900

    fix loss

--- ml/loss.py
+++ ml/loss.py
@@ -7,8 +7,8 @@ def simple_loss():
 
   def loss(y_true: list, y_pred: list, tag='train'):
     losses = [
-      cross_entropy(y_true[0], y_pred[0]),
-      binary_entropy(y_true[1], y_pred[1])
+      cross_entropy(y_true[0], tf.nn.softmax(y_pred[0])),
+      binary_entropy(y_true[1], tf.math.sigmoid(y_pred[1]))
     ]
     return losses
   return loss
